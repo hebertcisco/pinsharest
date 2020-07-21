1b6fe23ad9fc841d5a4437fc515b9d174273537f
tecnobert <mrrabbitomg@gmail.com>
2020-07-21 17:24:39 -0300

    Initial page generated.

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+
+import Form from "./components/form";
+import Nav from "./components/nav";
+import React from "react";
+import logo from "./img/badgeRGB.svg";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="main">
+          <Nav />
+          <img src={logo} className="App-logo" alt="logo" />
+          <p className="desc">Share a new Pin directly to {"Pinterest"}.</p>
+          <Form />
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
