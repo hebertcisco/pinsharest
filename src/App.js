cf8d9108dba88f4ed3ffe9e2f574877057461899
Hebert  F. Barros <mrrabbitomg@gmail.com>
2020-07-22 03:56:38 +0000

    Changing the path to styles.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./styles/css/App.css";
 
 import Form from "./components/form";
 import Nav from "./components/nav";
@@ -6,18 +6,18 @@ import React from "react";
 import logo from "./img/badgeRGB.svg";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div className="main">
-          <Nav />
-          <img src={logo} className="App-logo" alt="logo" />
-          <p className="desc">Share a new Pin directly to {"Pinterest"}.</p>
-          <Form />
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div className="main">
+                    <Nav />
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p className="desc">Share a new Pin directly to {"Pinterest"}.</p>
+                    <Form />
+                </div>
+            </header>
         </div>
-      </header>
-    </div>
-  );
+    );
 }
 
 export default App;
