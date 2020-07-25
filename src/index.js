00a3b8e78c3655702c5dada2c931d748c0e2b5cb
tecnobert <mrrabbitomg@gmail.com>
2020-07-25 03:07:53 -0300

    Avoiding global variables

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './styles/css/index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import "./styles/css/index.css";
+
+import * as serviceWorker from "./serviceWorker";
+
+import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom";
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.querySelector('[data-js="root"]')
 );
 
 serviceWorker.register();
